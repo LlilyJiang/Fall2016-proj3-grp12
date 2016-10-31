ace4a338fb5669bfb1f762853a12c6ee0d92d2f1
Lily Jiang <rongyue717@gmail.com>
2016-10-31 01:35:30 -0400

    add SGD logistic regression

--- lib/cross_validation.R
+++ lib/cross_validation.R
@@ -126,3 +126,36 @@ xg.cv.function <- function(X.train, y.train, d,n,r,p, K){
   return(mean(cv.error))
 }
 
+############ cv.function for SGD logistic regression ##########################################
+cvsgd.function <- function(data, label,K){
+  # data: the whole dataset
+  # label: a column vector with 0 and 1
+  # K: number of folds during the cross validation process
+
+  set.seed(0)
+  library(caret)
+  fold <- createFolds(1:dim(data)[1], K, list=T, returnTrain=F)
+  fold <- as.data.frame(fold)
+  
+  cv.error <- rep(NA, K)
+  
+  for (i in 1:K){
+    test.data <- data[fold[,i],]
+    train.data <- data[-fold[,i],]
+    test.label <- label[fold[,i],]
+    train.label <- label[-fold[,i],]
+    
+    
+    #fit <- sgd(train.data, train.label,model='glm',model.control=list(family="binomial",lambda2=0.001))
+    fit <- sgd(train.data, train.label,model='glm',model.control=binomial(link="logit"))
+    # print('trained')
+    pred <- predict(fit, test.data,type = 'response')  
+    pred <- ifelse(pred <= 0.5, 0, 1) 
+    cv.error[i] <- mean(pred != test.label)
+  }
+  
+  return(mean(cv.error))
+}
+
+
+
