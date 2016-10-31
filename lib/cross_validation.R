5a1c9441144511984572ad9e1cea79f7d91e03de
Jiwen You <jy2725@columbia.edu>
2016-10-31 17:17:39 -0400

    add adaboost

--- lib/cross_validation.R
+++ lib/cross_validation.R
@@ -4,6 +4,8 @@
 
 ### Project 3 Group 12
 
+################################ cv.function for GBM ########################################
+
 cv.function <- function(data, label, d, n, r, K){
   # data: the whole dataset
   # label: a column vector with 0 and 1
@@ -27,14 +29,46 @@ cv.function <- function(data, label, d, n, r, K){
     
     par <- list(depth = d, Ntrees = n, Shrinkage = r)
     fit <- train(train.data, train.label, par)
-    # print('trained')
     pred <- test(fit, test.data)  
-    # print('tested')
     cv.error[i] <- mean(pred != test.label)
   }
   
   return(mean(cv.error))
 }
+
+################################ cv.function for Adaboost ########################################
+
+cv.function <- function(data, label, d, n, r, K){
+  # data: the whole dataset
+  # label: a column vector with 0 and 1
+  # K: number of folds during the cross validation process
+  # d: depth_values
+  # n: Ntrees_values
+  # r: Shrinkage_values
+  
+  set.seed(0)
+  library(caret)
+  fold <- createFolds(1:dim(data)[1], K, list=T, returnTrain=F)
+  fold <- as.data.frame(fold)
+  
+  cv.error <- rep(NA, K)
+  
+  for (i in 1:K){
+    test.data <- data[fold[,i],]
+    train.data <- data[-fold[,i],]
+    test.label <- label[fold[,i],]
+    train.label <- label[-fold[,i],]
+    
+    par <- list(depth = d, Ntrees = n, Shrinkage = r)
+    fit <- train.ada(train.data, train.label, par)
+    pred <- test.ada(fit, test.data)  
+    cv.error[i] <- mean(pred != test.label)
+  }
+  
+  return(mean(cv.error))
+}
+
+################################ cv.function for xgboost ########################################
 
 cvsvm.function <- function(data, label, k, c, g, K){
   # data: the whole dataset
@@ -68,37 +102,7 @@ cvsvm.function <- function(data, label, k, c, g, K){
   return(mean(cv.error))
 }
 
-cvxgboost.function <- function(data, label, d, n, r, K){
-  # data: the whole dataset
-  # label: a column vector with 0 and 1
-  # K: number of folds during the cross validation process
-  # r: Shrinkage_values
-  
-  set.seed(0)
-  library(caret)
-  fold <- createFolds(1:dim(data)[1], K, list=T, returnTrain=F)
-  fold <- as.data.frame(fold)
-  
-  cv.error <- rep(NA, K)
-  
-  for (i in 1:K){
-    test.data <- data[fold[,i],]
-    train.data <- data[-fold[,i],]
-    test.label <- label[fold[,i],]
-    train.label <- label[-fold[,i],]
-    
-    par <- list(depth = d, Ntrees = n, Shrinkage = r)
-    fit <- trainxgboost(train.data, train.label, par)
-    # print('trained')
-    pred <- testxgboost(fit, test.data)  
-    # print('tested')
-    cv.error[i] <- mean(pred != test.label)
-  }
-  
-  return(mean(cv.error))
-}
-
-############ cv.function for xgboost ##########################################
+############################## cv.function for xgboost ##########################################
 
 xg.cv.function <- function(X.train, y.train, d,n,r,p, K){
   
@@ -126,7 +130,8 @@ xg.cv.function <- function(X.train, y.train, d,n,r,p, K){
   return(mean(cv.error))
 }
 
-############ cv.function for SGD logistic regression ##########################################
+########################## cv.function for SGD logistic regression ##########################################
+
 cvsgd.function <- function(data, label,K){
   # data: the whole dataset
   # label: a column vector with 0 and 1
