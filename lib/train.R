e6099159add789a943ec6cc7cce0098e1c315aad
Jingjing Feng <jf2961@columbia.edu>
2016-10-30 23:59:27 -0400

    add xg.train()

--- lib/train.R
+++ lib/train.R
@@ -107,25 +107,46 @@ trainSVM <- function(dat_train, label_train, par = NULL){
   return(fit = fit_svm)
 }
 
-trainxgboost <- function(dat_train, lable_train, par){
-  library(xgboost)
+############
+xg.train <- function(dat_train, label_train, par=NULL){
   
+  ### Train a Gradient Boosting Model (GBM) using processed features from training images
+  
+  ### Input: 
+  ###  -  processed features from images 
+  ###  -  class labels for training images
+  ### Output: training model specification
+  
+  ### load libraries
+  library("xgboost")
+  
+  ### Train with gradient boosting model
   if(is.null(par)){
-    ### default parameter values
-    depth <- 6
-    Ntrees <- 1000
-    Shrinkage <- 0.01
+    max_depth <- 6
+    nround <- 25
+    eta <- 0.3
+    colsample_bytree <- 1
   } else {
-    depth <- par$depth
-    Ntrees <- par$Ntrees
-    Shrinkage <- par$Shrinkage
-  } 
+    max_depth <- par$max_depth
+    nround <- par$nround
+    eta <- par$eta
+    colsample <- par$colsample
+  }
   
+  fit_xgb <- xgboost(data = dat_train, 
+                 label = label_train, 
+                 eta = eta,
+                 max_depth = max_depth, 
+                 nround=nround, 
+                 subsample = 0.5,
+                 colsample_bytree = colsample,
+                 seed = 9,
+                 eval_metric = "error",
+                 objective = "binary:logistic",
+                 verbose=FALSE
+  )
   
-  fit_xgboost <- xgboost(data = dat_train, label = label_train,
-                       max.depth = depth, eta = Shrinkage, nthread = Ntrees, 
-                       nround = 2, objective = "binary:logistic")
-  
-  return(fit = fit_xgboost)
+  return(list(fit=fit_xgb))
 }
+
 
