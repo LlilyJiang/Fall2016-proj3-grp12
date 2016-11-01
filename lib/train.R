ce601f076d879ebff05d3434ada19e2295c82aed
Jingjing Feng <jf2961@columbia.edu>
2016-11-01 19:42:06 -0400

    update

--- lib/train.R
+++ lib/train.R
@@ -6,50 +6,8 @@
 
 ################################################    Baseline Model   ########################################################
 
-### xg.train() for xgboost ###
+### new train function for xgboost: tune more parameters ###
 
-xg.train <- function(dat_train, label_train, par=NULL){
-  
-  ### Train a Gradient Boosting Model (GBM) using processed features from training images
-  
-  ### Input: 
-  ###  -  processed features from images 
-  ###  -  class labels for training images
-  ### Output: training model specification
-  
-  ### load libraries
-  library("xgboost")
-  
-  ### Train with gradient boosting model
-  if(is.null(par)){
-    max_depth <- 6
-    nround <- 25
-    eta <- 0.3
-    colsample_bytree <- 1
-  } else {
-    max_depth <- par$max_depth
-    nround <- par$nround
-    eta <- par$eta
-    colsample <- par$colsample
-  }
-  
-  fit_xgb <- xgboost(data = dat_train, 
-                 label = label_train, 
-                 eta = eta,
-                 max_depth = max_depth, 
-                 nround=nround, 
-                 subsample = 0.5,
-                 colsample_bytree = colsample,
-                 seed = 9,
-                 eval_metric = "error",
-                 objective = "binary:logistic",
-                 verbose=FALSE
-  )
-  
-  return(list(fit=fit_xgb))
-}
-
-################################### new train function for xgboost: tune more parameters ################
 xg.train.new <- function(dat_train, label_train, par=NULL){
   
   ### load libraries
@@ -84,3 +42,10 @@ xg.train.new <- function(dat_train, label_train, par=NULL){
 }
 
 ################################################    Baseline Model   ########################################################
+
+
+################################################    Advanced Model   ########################################################
+
+
+
+
