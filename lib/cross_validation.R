48c0062fa6f1b276fad4bb519bcdc3285bb9a326
Jingjing Feng <jf2961@columbia.edu>
2016-10-31 21:07:03 -0400

    delete cv.functions we tried but not need

--- lib/cross_validation.R
+++ lib/cross_validation.R
@@ -4,73 +4,9 @@
 
 ### Project 3 Group 12
 
-################################ cv.function for GBM ########################################
+######################################### cv.function for baseline model ################################################
 
-cv.function <- function(data, label, d, n, r, K){
-  # data: the whole dataset
-  # label: a column vector with 0 and 1
-  # K: number of folds during the cross validation process
-  # d: depth_values
-  # n: Ntrees_values
-  # r: Shrinkage_values
-
-  set.seed(0)
-  library(caret)
-  fold <- createFolds(1:dim(data)[1], K, list=T, returnTrain=F)
-  fold <- as.data.frame(fold)
-  
-  cv.error <- rep(NA, K)
-  
-  for (i in 1:K){
-    test.data <- data[fold[,i],]
-    train.data <- data[-fold[,i],]
-    test.label <- label[fold[,i],]
-    train.label <- label[-fold[,i],]
-    
-    par <- list(depth = d, Ntrees = n, Shrinkage = r)
-    fit <- train(train.data, train.label, par)
-    print("fit")
-    pred <- test(fit, test.data)  
-    print("test")
-    cv.error[i] <- mean(pred != test.label)
-  }
-  
-  return(mean(cv.error))
-}
-
-################################ cv.function for Adaboost ########################################
-
-cv.function <- function(data, label, d, n, r, K){
-  # data: the whole dataset
-  # label: a column vector with 0 and 1
-  # K: number of folds during the cross validation process
-  # d: depth_values
-  # n: Ntrees_values
-  # r: Shrinkage_values
-  
-  set.seed(0)
-  library(caret)
-  fold <- createFolds(1:dim(data)[1], K, list=T, returnTrain=F)
-  fold <- as.data.frame(fold)
-  
-  cv.error <- rep(NA, K)
-  
-  for (i in 1:K){
-    test.data <- data[fold[,i],]
-    train.data <- data[-fold[,i],]
-    test.label <- label[fold[,i],]
-    train.label <- label[-fold[,i],]
-    
-    par <- list(depth = d, Ntrees = n, Shrinkage = r)
-    fit <- train.ada(train.data, train.label, par)
-    pred <- test.ada(fit, test.data)  
-    cv.error[i] <- mean(pred != test.label)
-  }
-  
-  return(mean(cv.error))
-}
-
-############################## cv.function for xgboost ##########################################
+### cv.function for xgboost ###
 
 xg.cv.function <- function(X.train, y.train, d,n,r,p, K){
   
@@ -98,7 +34,42 @@ xg.cv.function <- function(X.train, y.train, d,n,r,p, K){
   return(mean(cv.error))
 }
 
-################# cv.function for SGD logistic regression ##########################################
+#### cv.function for new xgboost: use the function from xgboost package  ####
+
+# Simple cross validated xgboost training function (returning minimum error for grid search)
+# 5 fold
+
+xgbCV <- function (params) {
+  fit <- xgb.cv(
+    data = data.matrix(dat_train), 
+    label = label_train, 
+    param =params, 
+    missing = NA, 
+    nfold = folds, 
+    prediction = FALSE,
+    early.stop.round = 50,
+    maximize = FALSE,
+    nrounds = nrounds
+  )
+  rounds <- nrow(fit)
+  metric = paste('test.',eval,'.mean',sep='')
+  idx <- which.min(fit[,fit[[metric]]]) 
+  val <- fit[idx,][[metric]]
+  res <<- rbind(res,c(idx,val,rounds))
+  colnames(res) <<- c('idx','val','rounds')
+  return(val)
+}
+
+
+
+
+######################################### cv.function for baseline model ################################################
+
+
+
+######################################### cv.function for Advanced model ################################################
+
+### cv.function for SGD logistic regression ###
 
 cvsgd.function <- function(data, label,K){
   # data: the whole dataset
@@ -129,41 +100,5 @@ cvsgd.function <- function(data, label,K){
   
   return(mean(cv.error))
 }
-
-
-################# cv.function for new xgboost: use the function from xgboost package  ##########################################
-
-
-# Simple cross validated xgboost training function (returning minimum error for grid search)
-# 5 fold
-
-xgbCV <- function (params) {
-  fit <- xgb.cv(
-    data = data.matrix(dat_train), 
-    label = label_train, 
-    param =params, 
-    missing = NA, 
-    nfold = folds, 
-    prediction = FALSE,
-    early.stop.round = 50,
-    maximize = FALSE,
-    nrounds = nrounds
-  )
-  rounds <- nrow(fit)
-  metric = paste('test.',eval,'.mean',sep='')
-  idx <- which.min(fit[,fit[[metric]]]) 
-  val <- fit[idx,][[metric]]
-  res <<- rbind(res,c(idx,val,rounds))
-  colnames(res) <<- c('idx','val','rounds')
-  return(val)
-}
-
-
-
-
-
-
-
-
 
 
