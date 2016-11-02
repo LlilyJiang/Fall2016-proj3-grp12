18dab1c4f56002bca7e738528e0eafd0d0492725
Jingjing Feng <jf2961@columbia.edu>
2016-11-02 13:51:45 -0400

    add an optional model

--- lib/train.R
+++ lib/train.R
@@ -43,6 +43,44 @@ xg.train.new <- function(dat_train, label_train, par=NULL){
   return(list(fit=fit_xgb))
 }
 
+# optional base model
+xg.train.new.l <- function(dat_train, label_train, par=NULL){
+  
+  ### load libraries
+  library("xgboost")
+  
+  par1 = list(
+    eval_metric = "logloss",
+    objective = "binary:logistic",
+    # eta: Analogous to learning rate in GBM (Typical final values to be used: 0.01-0.2)
+    eta = 0.1,
+    max_depth = 6,
+    max_delta_step = 1,
+    subsample = 0.8,
+    scale_pos_weight = 1,
+    min_child_weight = 3
+  )
+  
+  if(is.null(par)){
+    par = par1
+  } else {
+    par = par
+  }
+  
+  fit_xgb <- xgboost(
+    data = xgb.DMatrix(data.matrix(dat_train),missing=NaN, label = label_train),
+    param = par,
+    # here nrounds is fixed
+    nrounds = 1000,
+    verbose=FALSE
+  )
+  
+  return(list(fit=fit_xgb))
+}
+
+
+
+
 
 ################################################    Advanced Model   ########################################################
 
