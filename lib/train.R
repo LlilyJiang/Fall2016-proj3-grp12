cbda55fe03ac322276d6fd47b44a563798927f07
Jingjing Feng <jf2961@columbia.edu>
2016-11-02 21:37:14 -0400

    modification

--- lib/train.R
+++ lib/train.R
@@ -2,24 +2,9 @@
 ### Train a classification model with training images ###
 #########################################################
 
-### Project 3 Group 12
-
-setwd("~Desktop") ## PLEASE set this directory containing 'Fall2016-proj3-grp12/' 
+### Project 3 Group 12 
 
 ################################################    Baseline Model   ########################################################
-nround = 25
-
-# Load training data
-sift.feature <- read.csv("sift_features.csv")
-data_train <- t(sift.feature)
-
-# Use PCA to reduce the dimension
-source("Fall2016-proj3-grp12/lib/feature_sift.r")
-data_train <- pca(data_train, 750)
-
-# Import the class label
-label_train <- matrix(c(rep(1,1000),rep(0,1000)),ncol = 1)
-
 # Baseline Model
 xg.train <- function(data_train, label_train, par=NULL){
   
@@ -51,66 +36,14 @@ xg.train <- function(data_train, label_train, par=NULL){
     param = par,
     # for here, nrounds is dependent, we should follow the code step by step
     # or you can change the nrounds = 1000
-    nrounds = nround,
+    nrounds = 1000,
     verbose = TRUE
   )
   
   return(fit_xgb)
 }
 
-fit1 = xg.train(data_train,label_train)
 
-###### cross validation procedure for the base line model ######
-
-# xgboost task parameters
-folds <- 5
-
-# Parameter grid to search
-eta = c(0.1,0.01)
-max_depth = c(4,6,8)
-max_delta_step = c(0,1)
-subsample = c(0.5,1)
-scale_pos_weight = c(0,1)
-min_child_weight = c(1,3,6)
-nround = 500
-
-result_cv <- array(dim=c(length(eta),length(max_depth),length(max_delta_step),
-                         length(subsample),length(scale_pos_weight),length(min_child_weight)))
-
-for(i in 1:length(eta)){
-  for(j in 1:length(max_depth)){
-    for(k in 1:length(max_delta_step)){
-      for (l in 1:length(subsample)){
-        for (m in 1:length(scale_pos_weight)){
-          for (n in 1:length(min_child_weight)){
-          model <- xgb.cv(data = data_train,label = label_train, eta = eta[i],
-                           max_depth = max_depth[j], max_delta_step = max_delta_step[k],
-                           subsample = subsample[l], sclae_pos_weight = scale_pos_weight[m],
-                           min_child_weight = min_child_weight[n], eval_metric = "error",
-                           objective = "binary:logistic", nfold = folds,
-                           nround = nround, verbose=TRUE)
-            rounds <- nrow(model)
-            metric <- paste('test.',eval,'.mean',sep='')
-            idx <- which.min(model[,model[[metric]]]) 
-            val <- model[idx,][[metric]]
-            result_cv[i,j,k,l,m,n] <- val
-          }
-        }
-      }
-    }
-  }
-}
-
-# select the best parameters by comparing testing error among all kinds of combinations
-index_best <- which(result_cv == min(result_cv), arr.ind = TRUE)
-eta.optimal <- eta[index_best[1]]
-max_depth.optimal <- max_depth[index_best[2]]
-max_delta_step.optimal <-max_delta_step[index_best[3]]
-subsample.optimal <- subsample[index_best[4]]
-scale_pos_weight.optimal <-scale_pos_weight[index_best[5]]
-min_child_weight.optimal<-min_child_weight[index_best[6]]
-
-save(result_cv, file="./output/err_cv_xgboost.RData")
 
 
 ################################################    Advanced Model   ########################################################
